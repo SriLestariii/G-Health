ba602efe19d16e115aaeb9c4de466cf47cf80109
SriLestariii <srilestari17@upi.edu>
2022-12-12 20:19:28 +0700

    G-Health team

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+kofiWidgetOverlay.draw('mohamedghulam', {
+  'type': 'floating-chat',
+  'floating-chat.donateButton.text': 'Support me',
+  'floating-chat.donateButton.background-color': '#323842',
+  'floating-chat.donateButton.text-color': '#fff'
+});
